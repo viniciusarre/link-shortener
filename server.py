ae12342484829532cf6304ba235f7629f38c1cd6
Vinícius <viniciusarre@gmail.com>
2020-09-30 07:42:25 -0300

    Improve redirecting functionality and overall flow for results page

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory, request, jsonify, render_template
+from flask import Flask, send_from_directory, request, jsonify, render_template, redirect
 from flask_pymongo import PyMongo
 import uuid
 
@@ -10,16 +10,18 @@ mongo = PyMongo(app)
 def shorten_from_form(form):
     _form = form
     _link = _form["url"]
+    if not("http" in _link or "https" in _link):
+        _link = "https://" + _link
     _count = 0
     _id = uuid.uuid4()
-    id = mongo.db.urls.insert(
+    mongo.db.urls.insert_one(
         {
             'link': _link,
             'endpoint': str(_id),
             'count': _count
         })
-    resp = jsonify({'message': 'Id successfully inserted!'})
-    resp.status_code = 200
+    resp = {'message': 'Id successfully inserted!',
+            'endpoint': str(_id), 'status_code': 200}
     return resp
 
 
@@ -35,7 +37,9 @@ def shorten():
 
 @app.route('/api/shorten', methods=["POST"])
 def shorten_url():
-    return shorten_from_form(request.form)
+    resp = jsonify(shorten_from_form(request.form))
+    resp.status_code = 200
+    return resp
 
 
 @app.route('/result', methods=["POST"])
@@ -44,9 +48,14 @@ def result():
     return render_template('result.html', result=result)
 
 
-@app.route('/s', methods=["GET"])
-def redirect():
-    return 'The link where the URL will be redirect to its original. The access counter for it will also be incremented'
+@app.route('/s/<handler>', methods=["GET"])
+def handle_redirecting(handler):
+    url = mongo.db.urls.find_one({'endpoint': handler})
+    mongo.db.urls.find_one_and_update(
+        {'endpoint': handler},
+        {'$inc': {'count': 1}}
+    )
+    return redirect(url['link'])
 
 
 if __name__ == "__main__":
